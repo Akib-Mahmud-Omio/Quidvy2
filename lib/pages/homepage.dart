b556f7f21ca8f0c25d5957c6e176901315657b4a
Reza066 <reza.cse.20220104066@aust.edu>
2023-12-27 00:20:16 +0600

    designed homepage ui

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -12,21 +12,159 @@ class _HomeState extends State<Home> {
     return Scaffold(
 
       body: SafeArea(
-      child: Stack(
-      children: [
-      Column(
-      children: [
-      Container(
-      width: double.infinity,
-      height: 240,
-      decoration: BoxDecoration(
-        color: Colors.deepOrange,
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(20),
-          bottomRight: Radius.circular(20),
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 240,
+                  decoration: BoxDecoration(
+                    color: Colors.deepOrange,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                    ),
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 35,
+                        left: 340,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(7),
+                          child: Container(
+                            height: 30,
+                            width: 40,
+                            color: Colors.deepOrange,
+                            child: Icon(
+                              Icons.notifications,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30, left: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Mr.x',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(height: 15),
+
+                            Text(
+                              'Total Balance:',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 14,
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(height: 5),
+                            Text(
+                              '\৳ 7260',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25,
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+
+                              children: [
+                                Row(
+
+                                  children: [
+                                    CircleAvatar(
+                                      radius: 13,
+                                      backgroundColor: Colors.deepOrange[400],
+                                      child: Icon(
+                                        Icons.arrow_downward,
+                                        color: Colors.white,
+                                        size: 19,
+                                      ),
+                                    ),
+                                    SizedBox(width: 7),
+                                    Text(
+                                      'Income',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 16,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(width: 180),
+                                Row(
+                                  children: [
+                                    CircleAvatar(
+                                      radius: 13,
+                                      backgroundColor: Colors.deepOrange[400],
+                                      child: Icon(
+                                        Icons.arrow_upward,
+                                        color: Colors.white,
+                                        size: 19,
+                                      ),
+                                    ),
+                                    SizedBox(width: 7),
+                                    Text(
+                                      'Expenses',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 16,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 6),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                SizedBox(width: 35),
+                                Text(
+                                  '\৳ 9560',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 17,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                SizedBox(width: 220),
+                                Text(
+                                  '\৳ 2300',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 17,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
-
     );
   }
 }
