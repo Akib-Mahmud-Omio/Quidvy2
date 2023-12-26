437466bc7cf58b6e5a9ec4b5f8bbc58f4f0cd57f
Reza066 <reza.cse.20220104066@aust.edu>
2023-12-26 23:54:32 +0600

    Added 5 icons

--- lib/pages/bottomnavigationbar.dart
+++ lib/pages/bottomnavigationbar.dart
@@ -21,6 +21,78 @@ class _BottomState extends State<Bottom> {
         backgroundColor: Color(0xfff25a18),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+    bottomNavigationBar: BottomAppBar(
+      height: 60,
+      shape: CircularNotchedRectangle(),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 4, bottom: 4),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  index = 0;
+                });
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(right: 25.0),
+                child: Icon(
+                  Icons.menu,
+                  size: 30,
+                  color: index == 0 ?  Color(0xfff25a18)  : Colors.blueGrey,
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  index = 1;
+                });
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0), // Added padding
+                child: Icon(
+                  Icons.pie_chart_rounded,
+                  size: 30,
+                  color: index == 1 ? Color(0xfff25a18) : Colors.blueGrey,
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  index = 2;
+                });
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 23.0), // Added padding
+                child: Icon(
+                  Icons.home,
+                  size: 30,
+                  color: index == 2 ?  Color(0xfff25a18) : Colors.blueGrey,
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  index = 3;
+                });
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 28.0), // Added padding
+                child: Icon(
+                  Icons.account_balance_wallet_outlined,
+                  size: 30,
+                  color: index == 3 ? Color(0xfff25a18) : Colors.blueGrey,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
 
     );
   }
