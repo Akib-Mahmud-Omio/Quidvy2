457205401ffe07b8ab627c517691fa131e3da7a5
Reza066 <reza.cse.20220104066@aust.edu>
2023-12-26 23:51:19 +0600

    Added navigationbar only

--- lib/pages/bottomnavigationbar.dart
+++ lib/pages/bottomnavigationbar.dart
@@ -7,9 +7,20 @@ class Bottom extends StatefulWidget {
   State<Bottom> createState() => _BottomState();
 }
 class _BottomState extends State<Bottom> {
+  int index = 2;
+  List Screen = [app_drawer(), Statistics(), Home(), Statistics()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Screen[index],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add,
+          size: 30,
+        ),
+        backgroundColor: Color(0xfff25a18),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
 
     );
   }
